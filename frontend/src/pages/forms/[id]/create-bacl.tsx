a3798abcab5e5069b7f4bd3d93ad1e4b183b7817
maceteligolden <maceteligolden@gmail.com>
2023-07-07 12:19:43 +0100

    feat: add create bacl page

---
+++ frontend/src/pages/forms/[id]/create-bacl.tsx
@@ -0,0 +1,17 @@
+import DashboardLayout from "@/common/layout/dashboard";
+import CreateBACLContent from "@/modules/form/create-bacl";
+import { useRouter } from "next/router";
+
+export default function CreateBACL() {
+    const router = useRouter();
+
+    return (
+        <>
+            <DashboardLayout>
+                {router.isReady &&
+                    <CreateBACLContent companyId={router.query.id}/>
+                }   
+            </DashboardLayout>
+        </>
+    )
+}
