7457bc19d2ce5599dd7fa6d6ddd255e250bb0d53
maceteligolden <maceteligolden@gmail.com>
2023-07-07 12:20:05 +0100

    feat: add create eepd page

---
+++ frontend/src/pages/forms/[id]/create-eepd.tsx
@@ -0,0 +1,17 @@
+import DashboardLayout from "@/common/layout/dashboard";
+import CreateEEPDContent from "@/modules/form/create-eepd";
+import { useRouter } from "next/router";
+
+export default function CreateEEBD() {
+    const router = useRouter();
+
+    return (
+        <>
+            <DashboardLayout>
+                {router.isReady &&
+                    <CreateEEPDContent companyId={router.query.id}/>
+                }   
+            </DashboardLayout>
+        </>
+    )
+}
