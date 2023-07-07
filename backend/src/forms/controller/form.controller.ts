1d83ce86838de1799ad5d4729dde50119e584958
maceteligolden <maceteligolden@gmail.com>
2023-07-07 12:22:53 +0100

    feat: add form controller

--- backend/src/forms/controller/form.controller.ts
+++ backend/src/forms/controller/form.controller.ts
@@ -1,14 +1,26 @@
 import { injectable } from "tsyringe";
 import { Request, Response } from "express";
 import CreateEEPDService from "../services/create_eepd.service";
+import CreateBACLService from "../services/create-bacl.service";
+import CreatePFECLService from "../services/create-pfecl.service";
 
 @injectable()
 export default class FormController {
     constructor(
         private createEEPDService: CreateEEPDService,
+        private createBACLService: CreateBACLService,
+        private createPFECLService: CreatePFECLService
     ){}
 
     async createEEBD(req: Request, res: Response){
         await this.createEEPDService.execute(req, res);
+    }
+
+    async createBACL(req: Request, res: Response){
+        await this.createBACLService.execute(req, res);
+    }
+
+    async createPFECL(req: Request, res: Response){
+        await this.createPFECLService.execute(req, res);
     }
 }
