49431d52a8f318500442165fd55fb9724d4ee6a6
maceteligolden <maceteligolden@gmail.com>
2023-07-07 12:20:22 +0100

    feat: add create pfecl page

---
+++ frontend/src/pages/forms/[id]/create-pfecl.tsx
@@ -0,0 +1,18 @@
+import DashboardLayout from "@/common/layout/dashboard";
+import CreateEEPDContent from "@/modules/form/create-eepd";
+import CreatePFECLContent from "@/modules/form/create-pfecl";
+import { useRouter } from "next/router";
+
+export default function CreatePFECL() {
+    const router = useRouter();
+
+    return (
+        <>
+            <DashboardLayout>
+                {router.isReady &&
+                    <CreatePFECLContent companyId={router.query.id}/>
+                }   
+            </DashboardLayout>
+        </>
+    )
+}
