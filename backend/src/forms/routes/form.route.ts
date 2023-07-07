af364b465e618f7dd1f21cdfe34ec608426d80f4
maceteligolden <maceteligolden@gmail.com>
2023-07-07 12:23:12 +0100

    feat; add create form endpoints

--- backend/src/forms/routes/form.route.ts
+++ backend/src/forms/routes/form.route.ts
@@ -6,5 +6,7 @@ const formRouter = Router();
 const formController = container.resolve(FormController);
 
 formRouter.post('/create-eepd', (req, res)=>formController.createEEBD(req, res));
+formRouter.post('/create-bacl', (req, res)=>formController.createBACL(req, res));
+formRouter.post('/create-pfecl', (req, res)=>formController.createPFECL(req, res));
 
 export default formRouter;
