c7b3a42fe2eaabe935f608209e485067096f8df3
maceteligolden <maceteligolden@gmail.com>
2023-07-11 11:11:43 +0100

    update: change frontend invoice model

--- frontend/src/common/model/invoice.model.ts
+++ frontend/src/common/model/invoice.model.ts
@@ -1,8 +1,6 @@
 import Base from "./base.model";
 
 export interface Invoice extends Base {
-  ticket?: string;
-  service?: string;
-  quantity?: number;
-  amount?: number;
+  ref_no?: string;
+  items?: any[];
 }
