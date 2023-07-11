6d3c9aa9320368cdc9eabf3efbfc79755bc4e2d4
maceteligolden <maceteligolden@gmail.com>
2023-07-11 11:01:52 +0100

    update: add new router to main routes

--- backend/src/common/router.ts
+++ backend/src/common/router.ts
@@ -4,6 +4,9 @@ import companyRouter from "../company/routes/company.route";
 import shipRouter from "../ship/routes/ship.route";
 import formRouter from "../forms/routes/form.route";
 import raftRouter from "../raft/routes/raft.routes";
+import invoiceRouter from "../invoice/routes/invoice.route";
+
+const BASE_URL = "/api/v2";
 
 export const setAppRouter = (app: Application) => {
     app.use("/api/v2/auth", authRouter);
@@ -11,4 +14,5 @@ export const setAppRouter = (app: Application) => {
     app.use("/api/v2/ships", shipRouter);
     app.use("/api/v2/forms", formRouter);
     app.use("/api/v2/rafts", raftRouter);
+    app.use(`${BASE_URL}/invoices`, invoiceRouter);
 }
