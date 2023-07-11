8dc9f4838deed7ef541db19a2042f55cde3b1fd4
maceteligolden <maceteligolden@gmail.com>
2023-07-11 10:59:12 +0100

    feat: add generateinvoice endpoint

---
+++ backend/src/invoice/service/generate-invoice.service.ts
@@ -0,0 +1,46 @@
+import { injectable } from "tsyringe";
+import { Request, Response } from "express";
+import IService from "../../common/interfaces/service.interface";
+import InvoiceRepository from "../../common/database/repository/invoice.repository";
+import Http from "../../common/helper/http.helper";
+import { generateReference } from "../../common/utils/generate-string";
+
+@injectable()
+export default class GenerateInvoiceService implements IService<Request, Response>{
+    constructor(
+        private httpHelper: Http,
+        private invoiceRepository: InvoiceRepository
+    ){
+
+    }
+
+    async execute(req: Request, res: Response) {
+        try{
+            const { items } = req.body;
+
+            const generatedInvReference = generateReference("INV");
+
+            const createInvoice = await this.invoiceRepository.addData({
+                ref_no: generatedInvReference
+            });
+
+            items.map((item: any)=> {
+                createInvoice.items?.push(item)
+            });
+
+            this.httpHelper.Response({
+                res,
+                status: "success",
+                message: "Successfully create invoice",
+                data: createInvoice
+            });
+
+        }catch(err: any){
+            this.httpHelper.Response({
+                res,
+                status: "error",
+                message: err.message
+            })
+        }
+    }
+}
