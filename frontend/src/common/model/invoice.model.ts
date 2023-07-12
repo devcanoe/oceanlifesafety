eb3b1fb85bb40b6f224ce1005d4b6ea90ee09b37
maceteligolden <maceteligolden@gmail.com>
2023-07-12 13:14:39 +0100

    feat: add invoiceitem interface

--- frontend/src/common/model/invoice.model.ts
+++ frontend/src/common/model/invoice.model.ts
@@ -1,6 +1,13 @@
 import Base from "./base.model";
 
+export interface InvoiceItem extends Base {
+  description?: string;
+  quantity?: number;
+  price?: number;
+  total?: number;
+}
+
 export interface Invoice extends Base {
   ref_no?: string;
-  items?: any[];
+  items?: InvoiceItem[];
 }
