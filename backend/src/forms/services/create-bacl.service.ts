2bf8523d116930a3e99a5c59a4c15e93054728f0
maceteligolden <maceteligolden@gmail.com>
2023-07-13 10:45:16 +0100

    update: add logs to form module

--- backend/src/forms/services/create-bacl.service.ts
+++ backend/src/forms/services/create-bacl.service.ts
@@ -4,12 +4,14 @@ import IService from "../../common/interfaces/service.interface";
 import Http from "../../common/helper/http.helper";
 import FormRepository from "../../common/database/repository/form.repository";
 import FormColumn from "../../common/database/models/form_columns.model";
+import LogRepository from "../../common/database/repository/log.repository";
 
 @injectable()
 export default class CreateBACLService implements IService<Request, Response>{
     constructor(
         private httpHelper: Http,
-        private formRepository: FormRepository
+        private formRepository: FormRepository,
+        private logRepository: LogRepository
     ){}
     async execute(req: Request, res: Response): Promise<void> {
         try{
@@ -20,7 +22,8 @@ export default class CreateBACLService implements IService<Request, Response>{
                 service_date,
                 flag_state,
                 last_service_date,
-                specifications
+                specifications,
+                user
             } = req.body;
 
             const createForm = await this.formRepository.addData({
@@ -35,6 +38,11 @@ export default class CreateBACLService implements IService<Request, Response>{
 
             specifications.map((specification: FormColumn)=>{
                 createForm.specifications?.push(specification)
+            });
+
+            await this.logRepository.addData({
+                description: `${user.email} created bacl form`,
+                user: user._id
             });
             
             this.httpHelper.Response({
--- backend/src/forms/services/delete-form.service.ts
+++ backend/src/forms/services/delete-form.service.ts
@@ -3,21 +3,30 @@ import { Request, Response } from "express";
 import FormRepository from "../../common/database/repository/form.repository";
 import Http from "../../common/helper/http.helper";
 import IService from "../../common/interfaces/service.interface";
+import LogRepository from "../../common/database/repository/log.repository";
 
 
 @injectable()
 export default class DeleteFormService implements IService<Request, Response> {
     constructor(
         private httpHelper: Http,
-        private formRepository: FormRepository
+        private formRepository: FormRepository,
+        private logRepository: LogRepository
     ){}
 
     async execute(req: Request, res: Response) {
         try{
 
             const {id} = req.params;
+
+            const { user } = req.body;
            
             await this.formRepository.deleteData(id);
+
+            await this.logRepository.addData({
+                description: `${user.email} deleted form with id ${id}`,
+                user: user._id
+            });
             
             this.httpHelper.Response({
                 res,
