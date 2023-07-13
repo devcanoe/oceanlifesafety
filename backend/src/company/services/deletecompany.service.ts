44239c2f600ecd6f683dbcd72ac30eed32336046
maceteligolden <maceteligolden@gmail.com>
2023-07-13 10:41:36 +0100

    feat: add logs to delete company

--- backend/src/company/services/deletecompany.service.ts
+++ backend/src/company/services/deletecompany.service.ts
@@ -3,11 +3,13 @@ import { Request, Response } from "express";
 import IService from "../../common/interfaces/service.interface";
 import Http from "../../common/helper/http.helper";
 import CompanyRepository from "../../common/database/repository/company.repository";
+import LogRepository from "../../common/database/repository/log.repository";
 
 @injectable()
 export default class DeleteCompanyService implements IService<Request, Response> {
     constructor(
         private companyRepository: CompanyRepository,
+        private logRepository: LogRepository,
         private httpHelper: Http
     ){
 
@@ -18,7 +20,14 @@ export default class DeleteCompanyService implements IService<Request, Response>
         
             const { id } = req.params;
 
+            const { user } = req.body;
+
             await this.companyRepository.deleteData(id);
+
+            await this.logRepository.addData({
+                description: `${user.email} deleted company with id ${id}`,
+                user: user._id
+            });
 
             this.httpHelper.Response({
                 res,
