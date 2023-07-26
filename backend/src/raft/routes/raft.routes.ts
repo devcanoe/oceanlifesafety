6b05411b95cd0ff8c4637e342210861fe64f0000
maceteligolden <maceteligolden@gmail.com>
2023-07-26 12:00:25 +0100

    update: add middleware to raft routes

--- backend/src/raft/routes/raft.routes.ts
+++ backend/src/raft/routes/raft.routes.ts
@@ -1,12 +1,13 @@
 import { Router } from "express";
 import { container } from 'tsyringe';
 import RaftController from "../controller/raft.controller";
+import userAuth from "../../common/middleware/authorization.middleware";
 
 const raftRouter = Router();
 const raftController = container.resolve(RaftController);
 
-raftRouter.post('/create', (req, res)=>raftController.createRaft(req, res));
-raftRouter.delete('/:id', (req, res)=>raftController.deleteRaft(req, res));
-raftRouter.get('/:id', (req, res)=>raftController.getRafts(req, res));
+raftRouter.post('/create', userAuth, (req, res)=>raftController.createRaft(req, res));
+raftRouter.delete('/:id', userAuth, (req, res)=>raftController.deleteRaft(req, res));
+raftRouter.get('/:id', userAuth, (req, res)=>raftController.getRafts(req, res));
 
 export default raftRouter;
