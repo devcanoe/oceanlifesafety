a1599a3e152fd0e82a4e96c217fb310aa03b0838
maceteligolden <maceteligolden@gmail.com>
2023-11-09 21:37:00 +0100

    update:api returns array

--- backend/src/calendar/services/get_highlights.service.ts
+++ backend/src/calendar/services/get_highlights.service.ts
@@ -1,5 +1,6 @@
 import { injectable } from "tsyringe";
 import CalendarRepository from "../../common/database/repository/calendar.repository";
+import Calendar from "../../common/database/models/calender.model";
 
 @injectable()
 export default class GetHighlightService {
@@ -10,6 +11,18 @@ export default class GetHighlightService {
     }
     
     async execute(data: Date){
-        return await this.calendarRepository.fetchData({ due_date: data.toISOString().split('T')[0]})
+        const response = await this.calendarRepository.fetchData({ due_date: data.toISOString().split('T')[0]});
+
+        let arrayMonthlyHighlight: number[] = []
+        
+        response.map((rows: Calendar)=> {
+            const dateString = rows.due_date; // Replace with your date string
+            const dateArray = dateString && dateString.split('-');
+            const day = dateArray && dateArray[2];
+
+            arrayMonthlyHighlight.push(Number(day))
+        })
+
+        return arrayMonthlyHighlight 
     }
 }
