43bc46bf6ceeb6a5348b095d221b303d519973ed
maceteligolden <maceteligolden@gmail.com>
2023-11-17 13:44:26 +0100

    feat:add search endpoint

--- backend/src/common/router.ts
+++ backend/src/common/router.ts
@@ -1,4 +1,4 @@
-import { Application } from "express";
+import { Application, Router } from "express";
 import authRouter, { dashboardRouter } from "../auth/routes/auth.route";
 import companyRouter from "../company/routes/company.route";
 import shipRouter from "../ship/routes/ship.route";
@@ -7,8 +7,13 @@ import raftRouter from "../raft/routes/raft.routes";
 import invoiceRouter from "../invoice/routes/invoice.route";
 import calendarRouter from "../calendar/routes/calendar.route";
 import userRouter from "../users/user.route";
+import userAuth from "./middleware/authorization.middleware";
+import InvoiceRepository from "./database/repository/invoice.repository";
+import { container } from "tsyringe";
+import Invoice from "./database/models/invoice.model";
 
 const BASE_URL = "/api/v2";
+const searchRouter = Router();
 
 export const setAppRouter = (app: Application) => {
     app.use("/api/v2/auth", authRouter);
@@ -20,4 +25,28 @@ export const setAppRouter = (app: Application) => {
     app.use(`${BASE_URL}/dashboard`, dashboardRouter);
     app.use(`${BASE_URL}/calendar`, calendarRouter);
     app.use(`${BASE_URL}/users`, userRouter);
+    app.use("/api/v2", searchRouter.post("/search", userAuth, async (req, res, next)=> {
+        try {
+            const { invoice_id } = req.body;
+            const invoiceRepository = container.resolve(InvoiceRepository);
+
+            let invoices_result: any[] = []
+
+            const invoices = await invoiceRepository.fetchData({
+                
+            })
+
+            invoices_result = invoices.filter((invoice: Invoice)=> {
+                return invoice.ref_no?.includes(invoice_id)
+            })
+
+            res.json({
+                status: "success",
+                message: "successfully get search results",
+                data: invoices_result
+            })
+        }catch(err){
+            next(err)
+        }
+    }));
 }
